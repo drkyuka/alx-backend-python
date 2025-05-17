2df9a7f6adddf8a76895ab9d9a2f5758796c15b8
Ibrahim Kyuka Avong <drkyavoni@live.com>
2025-05-17 18:03:25 +0100

    Change exception handling to catch specific ArithmeticError in average age computation

--- python-generators-0x00/4-main.py
+++ python-generators-0x00/4-main.py
@@ -5,5 +5,5 @@ average_age = __import__("4-stream_ages").compute_average_age
 try:
     average: float = average_age()
     print(f"Average age: {average}")
-except Exception as e:
+except ArithmeticError as e:
     print(f"Error: {e}")
