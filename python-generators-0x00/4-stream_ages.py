a635ae732725018019c2d9ea697a8d81b6364cb9
Ibrahim Kyuka Avong <drkyavoni@live.com>
2025-05-17 17:56:26 +0100

    Add initial implementation of average age computation and user streaming

---
+++ python-generators-0x00/4-stream_ages.py
@@ -0,0 +1,21 @@
+"""Generates a memory-efficient aggregate function i.e average age for a large dataset"""
+
+users = __import__("0-stream_users").stream_users
+
+def stream_user_ages():
+    """Generator function to stream user from the database."""
+    yield from users()
+
+def compute_average_age():
+    """Function to compute the average age of users."""
+    total_age = 0
+    count = 0
+
+    for user in stream_user_ages():
+        total_age += user[3]
+        count += 1
+
+    if count == 0:
+        return 0
+
+    return total_age / count
