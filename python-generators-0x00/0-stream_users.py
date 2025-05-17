02132a1a395b49b7fb43980c6788260b0c9b2dca
Ibrahim Kyuka Avong <drkyavoni@live.com>
2025-05-17 02:45:58 +0100

    Implement user streaming generator and main execution script

---
+++ python-generators-0x00/0-stream_users.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+# create a generator that streams rows from an SQL database one by one.
+seed = __import__("seed")
+
+def stream_users():
+    # function that uses a generator to fetch rows one by one from the user_data table
+
+    connection = seed.connect_to_prodev()
+    if connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM user_data;")
+        for row in cursor:
+            yield row
+        cursor.close()
+        connection.close()
+    else:
+        # Handle the case where the connection fails
+        print("Failed to connect to the database.")
---
+++ python-generators-0x00/1-main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+from itertools import islice
+stream_users_module = __import__('0-stream_users')
+
+# Use the function from the module
+for user in islice(stream_users_module.stream_users(), 6):
+    print(user)
