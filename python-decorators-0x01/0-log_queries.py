f6b0a73c1ede508c64ac22969ae1d09aef845be8
Ibrahim Kyuka Avong <drkyavoni@live.com>
2025-05-19 01:34:32 +0100

    Refactor log_queries decorator for improved structure and logging setup

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -2,50 +2,48 @@
 This script creates a decorator that logs database queries executed by any function
 """
 
-import sqlite3, functools, datetime
+import sqlite3, functools, datetime, logging
 
 #### decorator to lof SQL queries
-import logging
+logfile = f'dbqquery-{datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.log'
 logging.basicConfig(
     level=logging.INFO, 
     format='%(asctime)s - %(levelname)s - %(message)s',
     format="{asctime} - {levelname} - {message}",
     style="{",
     datefmt="%Y-%m-%d %H:%M",
+    filename=logfile,
+    filemode='a'
     )
 
-logfile = f'dbqquery{datetime.datetime.now()}.log'
-logging.FileHandler(logfile, mode='a')
 
 
-def log_queries():
+def log_queries(f):
     """
     Decorator to log SQL queries executed by the function
     """
-    def logging_decorator(f):
 
-        @functools.wraps(f)
-        def wrapper(*args, **kwargs):
-            query = args[0] if args else kwargs.get('query')
+    @functools.wraps(f)
+    def wrapper(*args, **kwargs):            
+        query = args[0] if args else kwargs.get('query')
 
-            # Check if the query is provided
-            if not query:
-                logging.error("No query provided to log.")
-                return None
+        # Check if the query is provided
+        if not query:
+            logging.error("No query provided to log.")
+            return None
             
-            logging.info(f"Query to be exexuted: {query}")
-            try:
-                result = f(*args, **kwargs)
-                logging.info(f"Query executed successfully: {query}")
-            except Exception as e:
-                logging.error(f"Error executing query: {query}")
-                logging.exception(e)
-                return None
+        logging.info(f"Query to be exexuted: {query}")
+        try:
+            result = f(*args, **kwargs)
+            logging.info(f"Query executed successfully: {query}")
+        except Exception as e:
+            logging.error(f"Error executing query: {query}")
+            logging.exception(e)
+            return None
 
-            # Call the original function
-            return result
-        return wrapper
-    return logging_decorator
+        # Call the original function
+        return result
+    return wrapper
 
 
 @log_queries
