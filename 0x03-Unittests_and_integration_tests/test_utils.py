9a23cf7450f1e444e7ee17fb5c89161f800fac6d
Ibrahim Kyuka Avong <drkyavoni@live.com>
2025-05-27 00:30:36 +0100

    Add utility functions and unit tests for nested map access
    - Implemented `access_nested_map` function to retrieve values from nested dictionaries using a sequence of keys.
    - Created `get_json` function to fetch JSON data from a given URL.
    - Added `memoize` decorator to cache method results for improved performance.
    - Developed unit tests for `access_nested_map` to validate correct behavior with existing keys and handle missing keys.
    - Included example usage of `access_nested_map` in the `utils.py` file.

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""
+This script contains utility functions for testing purposes.
+It includes function for Unittests and Integration Tests
+"""
+
+import unittest
+from typing import Any, Union
+from parameterized import parameterized
+
+access_nested_map = __import__("utils").access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """
+    Test case for the access_nested_map function.
+    """
+
+    @parameterized.expand(  # type: ignore
+        [
+            ({"a": 1}, ("a",), 1),
+            ({"a": {"b": 2}}, ("a",), {"b": 2}),
+            ({"a": {"b": 2}}, ("a", "b"), 2),
+        ]
+    )
+    def test_access_nested_map(
+        self,
+        nested_map: dict[str, Any],
+        path: tuple[str, ...],
+        expected: Union[int, dict[str, int]],
+    ) -> None:
+        """
+        Test accessing a nested map with valid keys.
+        """
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand(  # type: ignore
+        [({}, ("a",)), ({"a": 1}, ("a", "b"))]
+    )
+    def test_access_nested_map_missing_key(
+        self,
+        nested_map: dict[str, Any],
+        path: tuple[str, ...],
+    ) -> None:
+        """
+        Test accessing a nested map with a missing key.
+        """
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
