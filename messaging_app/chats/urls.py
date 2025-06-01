0c9aaaff61443bb7bd5b6f2b1b1c239b0fd52069
Ibrahim Kyuka Avong <drkyavoni@live.com>
2025-06-01 18:10:15 +0100

    Add django-rest-framework-nested package and update URL routing for conversations and messages

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -4,16 +4,25 @@ This file defines the URL patterns for the chats application,
 including the endpoints for managing conversations, messages, and users.
 """
 
-from django.db import router
+from rest_framework_nested.routers import NestedSimpleRouter as NestedDefaultRouter
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ConversationViewSet, UserViewSet, MessageViewSet
+from .views import ConversationViewSet, MessageViewSet
 
 
 router = DefaultRouter()
 router.register(r"conversations", ConversationViewSet, basename="conversation")
-router.register(r"users", UserViewSet, basename="user")
 router.register(r"messages", MessageViewSet, basename="message")
+
+
+conversations_router = NestedDefaultRouter(
+    router, r"conversations", lookup="conversation"
+)
+conversations_router.register(
+    r"messages", MessageViewSet, basename="conversation-messages"
+)
+
 urlpatterns = [
     path("", include(router.urls)),
+    path("", include(conversations_router.urls)),
 ]
