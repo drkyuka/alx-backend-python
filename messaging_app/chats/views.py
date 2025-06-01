a74f9cab1a2bb85222d81dbf6b20342b9ffc0224
Ibrahim Kyuka Avong <drkyavoni@live.com>
2025-06-01 17:41:38 +0100

    Add URL configuration for chats application and enhance ConversationViewSet

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+"""views.py
+This file contains the ViewSets for the chats application.
+"""
+
+from rest_framework import viewsets
+from .models import Conversation
+from .serializers import UserSerializer, MessageSerializer, ConversationSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
 # Create your views here.
+
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for managing conversations.
+    Provides CRUD operations for Conversation model.
+    """
+
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+
+    def get_queryset(self):
+        """
+        Override to filter conversations based on the authenticated user.
+        """
+        return self.queryset.filter(
+            participants__user_id=self.request.user.user_id
+        ).distinct()
