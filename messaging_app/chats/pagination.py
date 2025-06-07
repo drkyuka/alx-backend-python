a80bd7885a0d7cc4839286b5aa36ef526fc9cd52
Ibrahim Kyuka Avong <drkyavoni@live.com>
2025-06-07 12:33:46 +0100

    style: Improve docstring formatting in pagination module for clarity

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -1,9 +1,13 @@
 '''pagination.py
-This module defines a custom pagination class for handling message sets in the messaging application.
-It extends the `PageNumberPagination` class from Django REST Framework to provide pagination functionality for messages.
+This module defines a custom pagination class
+for handling message sets in the messaging application.
+It extends the `PageNumberPagination` class
+from Django REST Framework to provide pagination
+functionality for messages.
 """'''
 
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 
 class MessageSetPagination(PageNumberPagination):
@@ -14,3 +18,15 @@ class MessageSetPagination(PageNumberPagination):
 
     page_size = 20  # Default number of messages per page
     page_size_query_param = "page_size"
+
+    def get_paginated_response(self, data):
+        return Response(
+            {
+                "count": self.page.paginator.count,
+                "total_pages": self.page.paginator.num_pages,
+                "current_page": self.page.number,
+                "next": self.get_next_link(),
+                "previous": self.get_previous_link(),
+                "results": data,
+            }
+        )
